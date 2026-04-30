68c5407d9baad8cdbd828695d8d7ef4acbec785f
oliver-dev-star <oliveryyurany@gmail.com>
2026-04-30 11:26:35 -0500

    Initial upload of Software FJ exception system
    add all the exceptios system.

---
+++ exceptions.py
@@ -0,0 +1,68 @@
+#General Errors
+
+class AppError(Exception):
+    pass
+
+
+# Costumers
+
+class CustomerNameError(Exception):
+    pass
+
+class CustomerIDError(Exception):
+    pass    
+
+class EmailError(Exception):
+    pass
+
+class PhoneError(Exception):
+    pass
+
+
+#Services
+
+
+class ServiceError(Exception):
+    pass
+
+class ServiceNameError(Exception):
+    pass
+
+class ServiceIDError(Exception):
+    pass
+
+class ServicePriceError(Exception):
+    pass
+
+class ServiceUnavailableError(Exception):
+    pass
+
+
+#Reservations
+
+class ReservationError(Exception):
+    pass
+
+class ReservationIDError(Exception):
+    pass
+
+class ReservationStatusError(Exception):
+    pass
+
+class ReservationDateError(Exception):
+    pass
+
+class ReservationQuantityError(Exception):
+    pass
+
+class ReservationServiceError(Exception):
+    pass
+
+class ReservationCustomerError(Exception):
+    pass
+
+class ReservationNotFoundError(Exception):
+    pass
+
+class DurationError(Exception):
+    pass
